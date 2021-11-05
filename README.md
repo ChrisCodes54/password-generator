f091d86f2b8a294024d1f3124090e169c8af0d0b
ChrisCodes54 <christangarife95@gmail.com>
2021-11-05 16:53:10 -0400

    finished the logic section, added the randomizer, and added a reset for password and userchoices

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# password-generator
+# Password Generator #
+
+  ## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Questions](#questions)
+
+## Description
+The password generator has been created in order to ensure well protected passwords for any user. With secure passwords being increasingly more essential, the password generator allows for users to set their own criteria of a password and recieve an entirely randomized password. Thus making it harder to crack for anyone trying to infiltrate an account.
+## Install
+No install needed.
+## Usage
+To create randomized password to protect people's accounts/ 
+## License
+No license.
+## Contributors
+Christopher Tangarife
+## Questions
+If you have any questions about the application contact me directly at Christangarife95@gmailcom 
+# Contact
+Christangarife95@gmail.com 
+# Username
+Check out my other work on [Github](https://github.com/ChrisCodes54)
--- script.js
+++ script.js
@@ -1,50 +1,77 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var characters =  ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
+var password = "";
+var userChoices = "";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var characters = "!@#$%^&*()_+[]{}:;";
 
 // Write password to the #password input
 
+function writePassword() {
+  password = ""; 
+  userChoices = "";
+  var lengthinput = parseInt(
+    prompt(
+      "How long would you like your password to be? (Must be between 8 and 128)"
+    )
+  );
 
-function generatePassword(){
-  var lengthinput = prompt("How long would you like your password to be? (Must be between 8 and 128)")
   if (lengthinput < 8 || lengthinput > 128) {
-    alert("Must be between 8 and 128 characters")
-    return generatePassword();
+    alert("Must be between 8 and 128 characters");
+    return writePassword();
   }
 
-  var lowerinput = confirm("Will this contain lower case letters?")
-  var upperinput = confirm("Will this contain upper case letters?")
-  var numberinput = confirm("Will this contain numbers?")
-  var specialinput = confirm("Will this contain special characters?")
+  console.log(lengthinput);
+
+  var lowerinput = confirm("Will this contain lower case letters?");
+  if (lowerinput === true) {
+    userChoices += lowercase;
+    // userChoices + lowercase = userChoices;
+  }
+  console.log(lowerinput);
+
+  var upperinput = confirm("Will this contain upper case letters?");
+  if (upperinput === true) {
+    userChoices += uppercase;
+  }
+  console.log(upperinput);
+
+  var numberinput = confirm("Will this contain numbers?");
+  if (numberinput === true) {
+    userChoices +=numbers;
+  }
+  
+  console.log(numberinput);
+
+  var specialinput = confirm("Will this contain special characters?");
+  if (specialinput === true) {
+    userChoices += characters;
+  }
+  
+  console.log(specialinput);
+
+  console.log(userChoices);
 
   if (!lowerinput && !upperinput && !numberinput && !specialinput) {
-    alert("You must choose at least 1 criteria")
-    return generatePassword();
+    alert("You must choose at least 1 criteria");
+    return writePassword();
+  } else {
+    console.log("All clear to make the password")
+
+    for(var i = 0; i < lengthinput; i++){
+      
+      var randomizer = Math.floor(userChoices.length * Math.random()) 
+      console.log("randomizer", randomizer);
+      password += userChoices.charAt(randomizer);
+      console.log(password);
+    }
   }
-
-  var randomp = []; 
-  // need to check what has been selected
-
-  if (lowerinput) {
-    // add the lowercase letter to our array, combine array. look up how to make a string to array. 
-  }
-
-}
-
-
-function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
 
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
